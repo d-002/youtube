b20cb379fb9d8a9b13a2b4dfdc42c3d42c04986a
d-002 <minecraftenjoy@outlook.fr>
2025-07-05 15:50:17 +0200

    more versatile voronoi wrapper

--- Manim/main.py
+++ Manim/main.py
@@ -1,21 +1,24 @@
-from manim import Scene, Polygon, VGroup, ValueTracker, RED, BLUE_E
+from manim import Scene, Square, Polygon, VGroup, ValueTracker, RED, BLUE_E
 from fast_voronoi import *
 from fast_voronoi.polygons import make_polygons
 
-# todo: use only vgroup, animate
+#from manim import config
+#config.transparent = True
 
 class VoronoiAnim:
     def __init__(self, options: Options, bounds: Bounds, cells: list[Cell],
-                 colors: list[tuple], buffer: int = 10, **kwargs):
+                 colors: list[tuple], style_func, buffer: int = 10):
         self.options = options
         self.bounds = bounds
         self.cells = cells
         self.colors = colors
 
+        self.style_func = style_func
+
         # could be a varying number of polygons per cell, so add a small buffer
         length = len(cells)+buffer
         dummy = [(-1, 0, 0), (1, 0, 0), (0, 1.41, 0)]
-        self.vgroup = VGroup(Polygon(*dummy, **kwargs) for _ in range(length))
+        self.vgroup = VGroup(Polygon(*dummy) for _ in range(length))
         self._set_points()
 
     def _set_points(self):
@@ -30,9 +33,8 @@ class VoronoiAnim:
             polygon = self.vgroup[index]
 
             polygon.set_points_as_corners(points)
-            polygon.set_stroke(self.colors[i])
-            polygon.set_fill(self.colors[i])
-            polygon.set_opacity(1)
+            polygon.set_opacity(1) # fallback
+            self.style_func(polygon, self.colors[i])
 
             index += 1
 
@@ -52,6 +54,14 @@ class VoronoiAnim:
 
 class Main(Scene):
     def construct(self):
+        def style_poly_transparent(polygon: Polygon, color: tuple):
+            polygon.set_stroke(color)
+            polygon.set_fill(color, opacity=.5)
+
+        def style_poly_fill(polygon: Polygon, color: tuple):
+            polygon.set_stroke(width=0)
+            polygon.set_fill(color, opacity=1)
+
         def updater_swap_weights(voronoi, t):
             c1, c2 = voronoi.cells
             c1.weight = 1+t
@@ -61,8 +71,7 @@ class Main(Scene):
         bounds = Bounds(-5, -4, 10, 8)
         options = Options(segments_density=10, divide_lines=True)
         colors = [RED, BLUE_E]
-        voronoi = VoronoiAnim(options, bounds, cells, colors, stroke_width=0)
+        voronoi = VoronoiAnim(options, bounds, cells, colors, style_poly_fill)
 
         self.add(voronoi.vgroup)
         voronoi.play(self, updater_swap_weights, run_time=3)
-        self.wait(1)

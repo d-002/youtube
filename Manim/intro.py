414b2813290c40f0dda744465b7547e0253188a5
d-002 <minecraftenjoy@outlook.fr>
2025-07-07 11:21:57 +0200

    done with intro

--- Manim/intro.py
+++ Manim/intro.py
@@ -23,8 +23,7 @@ class VoronoiAnim:
 
         if show_dots:
             self.dots = VGroup(*(Dot((cell.pos.x, cell.pos.y, 0),
-                                     radius=.2)
-                                       for cell in cells))
+                                     radius=.2) for cell in cells))
         else:
             self.dots = VGroup()
 
@@ -41,9 +40,13 @@ class VoronoiAnim:
                 raise ValueError('Not enough polygons in VoronoiAnim buffer')
             polygon = self.polygons[index]
 
-            polygon.set_points_as_corners(dots)
-            polygon.set_opacity(1) # fallback
-            self.style_polygon(polygon, self.colors[i])
+            # hard-code hide polygon if too small
+            if len(dots) < 5:
+                polygon.set_opacity(0)
+            else:
+                polygon.set_points_as_corners(dots)
+                polygon.set_opacity(1) # fallback
+                self.style_polygon(polygon, self.colors[i])
 
             index += 1
 
@@ -72,14 +75,14 @@ class Dance:
         cx, cy = camera.frame_center[:2]
         w, h = camera.frame_width, camera.frame_height
         x, y = cx-w/2, cy-h/2
-        x, y, w, h = x-10, y-10, w+20, h+20
+        x, y, w, h = x-20, y-20, w+40, h+40
 
         self.bounds = Bounds(x, y, w, h)
 
     def init1(self, voronoi):
         black, white = voronoi.cells[:2]
-        black.pos = v2(self.bounds.right/2, 0)
-        white.pos = v2(self.bounds.left, 0)
+        black.pos = v2(self.bounds.right/4, 0)
+        white.pos = v2(self.bounds.left*.7, 0)
         black.weight = 1
         white.weight = 1
 
@@ -92,29 +95,29 @@ class Dance:
 
     def arrive1(self, voronoi, t):
         black, white = voronoi.cells[:2]
-        black.pos.x = (1-t) * self.bounds.right
+        black.pos.x = (1-t) * self.bounds.right/2
         white.weight = 5 - 4*t
 
     def arrive2(self, voronoi, t):
         black, white = voronoi.cells[:2]
-        black.pos.x = t * (.2*self.bounds.right)
-        white.pos.x = (1-t*.8) * self.bounds.left
+        black.pos.x = t * (.1*self.bounds.right)
+        white.pos.x = (.7-t*.6) * self.bounds.left
 
     def close(self, voronoi, t):
         black, white = voronoi.cells[:2]
-        black.pos.x = (t*.1 + .2)*self.bounds.right
-        white.pos.x = (1-t) * (.2*self.bounds.left)
+        black.pos.x = (t*.05 + .1)*self.bounds.right
+        white.pos.x = (1-t) * (.1*self.bounds.left)
 
     def updown(self, voronoi, t):
         white = voronoi.cells[1]
-        white.pos.y = t * (.2*self.bounds.bottom)
+        white.pos.y = t * (.1*self.bounds.bottom)
 
     def rotate(self, voronoi, t):
         black, white = voronoi.cells[:2]
 
         a1 = (3*pi) * max(0, t*1.05 - .05) # black (lag)
         a2 = 3*pi*t + pi # white
-        radius = .2*self.bounds.right
+        radius = .1*self.bounds.right
 
         black.pos = v2(cos(a1), -sin(a1))*radius
         white.pos = v2(cos(a2), -sin(a2))*radius
@@ -122,45 +125,97 @@ class Dance:
     def init3(self, voronoi):
         black, white, green = voronoi.cells[:3]
 
-        black.pos = v2(.2*self.bounds.left, 0)
-        white.pos = v2(.2*self.bounds.right, 0)
+        black.pos = v2(.1*self.bounds.left, 0)
+        white.pos = v2(.1*self.bounds.right, 0)
 
         green.weight = 1
-        green.pos = v2(0, self.bounds.bottom)
+        green.pos = v2(0, self.bounds.top/2)
         voronoi.update()
 
     def arrive3(self, voronoi, t):
         black, white, green = voronoi.cells[:3]
 
-        y = t * .15*self.bounds.bottom
+        y = t * .075*self.bounds.bottom
         black.pos.y = y
         white.pos.y = y
 
-        green.pos.y = (1 - t*.9) * self.bounds.top
+        green.pos.y = (1 - t*.9) * self.bounds.top/2
 
     def cut(self, voronoi, t):
         black, white, green = voronoi.cells[:3]
 
-        y = (1 - 2*t) * .15*self.bounds.bottom
+        y = (1 - 2*t) * .075*self.bounds.bottom
         black.pos.y = y
         white.pos.y = y
 
-        green.pos.y = (1 - 2*t) * .1*self.bounds.top
+        green.pos.y = (1 - 2*t) * .05*self.bounds.top
 
     def init_all(self, voronoi):
         seed(0)
 
-        for i in range(3, len(voronoi.cells)):
-            cell = voronoi.cells[i]
+        for cell in voronoi.cells[3:]:
             cell.weight = 1
 
             x, y = random()-.5, random()-.5
             x += -.3 if x < 0 else .3
             y += -.3 if y < 0 else .3
-            cell.pos = v2(x*self.bounds.right, y*self.bounds.bottom)
+            cell.pos = v2(x*self.bounds.right/2, y*self.bounds.bottom/2)
+
+        # ugly but works
+        for cell in voronoi.cells:
+            cell.base_pos = cell.pos
 
         voronoi.update()
 
+    def arrive_everything(self, voronoi, t):
+        scale = 1 / (1+t)
+        for cell in voronoi.cells:
+            cell.pos = cell.base_pos*scale
+
+    def weight(self, voronoi, t):
+        for i, cell in enumerate(voronoi.cells):
+            if i == 0:
+                weight = 5
+            elif i == 1:
+                weight = 1
+            else:
+                weight = 10
+
+            cell.weight = t * (weight-1) + 1
+
+    def disappear_other(self, voronoi, t):
+        weight = 10 + 100*t
+        opacity = 1-t
+
+        for cell in voronoi.cells[2:]:
+            cell.weight = weight
+        for dot in voronoi.dots[2:]:
+            dot.set_opacity(opacity)
+
+    def init_swap(self, voronoi):
+        for cell in voronoi.cells[:2]:
+            cell.base_pos = cell.pos
+
+        for i in range(len(voronoi.cells)):
+            cell = voronoi.cells[i]
+            cell.pos = v2(self.bounds.left, i)
+
+    def swap(self, voronoi, t):
+        black, white = voronoi.cells[:2]
+        black.weight = 5 - t*4
+        white.weight = 1 + t*4
+
+        offset = white.base_pos * -t
+        opacity = 1-t
+
+        for cell in voronoi.cells[:2]:
+            cell.pos = cell.base_pos + offset
+        for dot in voronoi.dots[:2]:
+            dot.set_opacity(opacity)
+
+    def disappear_last(self, voronoi, t):
+        white = voronoi.cells[1]
+        white.weight = 5 + 100*t
 
 class Intro(Scene):
     def construct(self):
@@ -186,29 +241,28 @@ class Intro(Scene):
 
             dot.set_color(ManimColor.from_rgb((r, g, b)))
 
+        def style_none(_, __):
+            pass
+
         dance = Dance(self.camera)
         bounds = dance.bounds
 
         options = Options(segments_density=10, divide_lines=True)
         cells = [Cell(v2(i, 0), 1) for i in range(8)]
-        colors = [BLACK, WHITE, GREEN, YELLOW, BLUE, RED, PURPLE, ORANGE]
+        colors = [BLACK, WHITE, GRAY_B, YELLOW, BLUE, RED, GREEN, ORANGE]
 
         funcs = style_poly_fill, style_dot_invert
         voronoi = VoronoiAnim(options, bounds, cells, colors, funcs, True)
 
-        """
-        text = Text('Headphones recommended',
-                    font_size=20)
-        self.play(Write(text, run_time=2))
+        text1 = Text('Headphones recommended', font_size=20)
+        self.play(Write(text1, run_time=2))
         self.wait(1)
-        self.play(FadeOut(text, run_time=5))
-        """
+        self.play(FadeOut(text1, run_time=5))
 
         self.add(voronoi.polygons)
         self.add(voronoi.dots)
         dance.init1(voronoi)
 
-        """
         voronoi.play(self, dance.arrive1, run_time=2, rate_func=rush_from)
         self.wait(1.5)
         voronoi.play(self, dance.arrive2, run_time=4, rate_func=double_smooth)
@@ -220,22 +274,36 @@ class Intro(Scene):
         voronoi.play(self, dance.updown, run_time=4, rate_func=there_and_back)
         self.wait(1.5)
         voronoi.play(self, dance.rotate, run_time=5)
-        self.wait(1.5)
-        """
+        text2 = Text('A presentation by D_00', font_size=30)
+        text2.to_corner(UL)
+        self.play(Create(text2), run_time=1.5)
 
         dance.init3(voronoi)
 
-        voronoi.play(self, dance.arrive3, run_time=5, rate_func=double_smooth)
+        voronoi.play(self, dance.arrive3, run_time=4, rate_func=double_smooth)
+        self.play(FadeOut(text2), run_time=1)
         voronoi.play(self, dance.cut, run_time=5, rate_func=there_and_back)
         self.wait(.5)
 
         everything = VGroup(voronoi.polygons, voronoi.dots)
-        # ok I give up for this animation
         self.play(FadeOut(everything), run_time=1)
         dance.init_all(voronoi)
         self.play(FadeIn(everything), run_time=1)
         self.wait(1)
-        self.play(FadeOut(everything), run_time=1)
-        everything.scale(.45)
-        self.play(FadeIn(everything), run_time=1)
+        voronoi.play(self, dance.arrive_everything, run_time=3)
         self.wait(2)
+
+        voronoi.play(self, dance.weight, run_time=7)
+        voronoi.style_poly = voronoi.style_dot = style_none
+        voronoi.play(self, dance.disappear_other, run_time=1)
+        voronoi.style_poly, voronoi.style_dot = style_poly_fill, style_dot_invert
+        self.wait(.5)
+        dance.init_swap(voronoi)
+        voronoi.play(self, dance.swap, run_time=3)
+        title = VGroup(Text('The beauty of', font_size=40).shift(UP),
+                       Text('Voronoi diagrams', font_size=40).shift(DOWN),
+                       Text('#SOME4', color=GRAY, font_size=20).shift(2*DOWN))
+        self.play(Write(title), run_time=1.5, lag_ratio=.5)
+        voronoi.play(self, dance.disappear_last, run_time=1.5, rate_func=lambda t: 1-slow_into(1-t))
+        self.wait(.5)
+        self.play(FadeOut(title), run_time=2)

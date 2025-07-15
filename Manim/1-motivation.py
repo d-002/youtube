ad03c2e8a0aa82732ec315f9d9e434c4f4feea12
d-002 <minecraftenjoy@outlook.fr>
2025-07-15 17:45:52 +0200

    using svg for images

--- Manim/1-motivation.py
+++ Manim/1-motivation.py
@@ -4,4 +4,5 @@ from theme import *
 
 class Main(Scene):
     def construct(self):
-        pass
+        img = SVGMobject("resources/voronoy.svg").scale(4)
+        self.play(Create(img), run_time=5, rate_func=linear)
